29576f41ca12d124b2719f390c7a2b7f8109c4e4
Brian W. Wolter <brian@brianwolter.com>
2020-02-14 13:33:47 -0500

    Adding httputil

---
+++ v1/httputil/httputil.go
@@ -0,0 +1,56 @@
+package httputil
+
+import (
+	"encoding/json"
+	"fmt"
+	"mime"
+	"net/http"
+	"strings"
+
+	"github.com/bww/go-rest/v1/errors"
+
+	"github.com/bww/go-router/v1"
+	"github.com/gorilla/schema"
+)
+
+var ErrUnsupportedMimetype = fmt.Errorf("Unsupported content type")
+
+var formDecoder *schema.Decoder
+
+func init() {
+	formDecoder = schema.NewDecoder()
+	formDecoder.IgnoreUnknownKeys(true)
+}
+
+// Unmarshal common entity types:
+//  - application/json
+//  - application/x-www-form-urlencoded
+//  - multipart/form-data
+func Unmarshal(req *router.Request, entity interface{}) error {
+	m, _, err := mime.ParseMediaType(req.Header.Get("Content-Type"))
+	if err != nil {
+		return err
+	}
+	switch strings.ToLower(m) {
+
+	case "application/json":
+		err := json.NewDecoder(req.Body).Decode(entity)
+		if err != nil {
+			return fmt.Errorf("Could not unmarshal request entity: %v", err)
+		}
+
+	case "application/x-www-form-urlencoded", "multipart/form-data":
+		err := (*http.Request)(req).ParseForm()
+		if err != nil {
+			return fmt.Errorf("Could not parse payload: %v", err)
+		}
+		err = formDecoder.Decode(entity, req.Form)
+		if err != nil {
+			return fmt.Errorf("Could not unmarshal request entity: %v", err)
+		}
+
+	default:
+		return ErrUnsupportedMimetype
+
+	}
+}
